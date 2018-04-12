2299bbe6a41e302f71a634166b75caacfd26df23
CristianRodriguezPena <cristianrodriguez@Interapts-MacBook-Pro-141.local>
2018-04-12 14:50:06 -0400

    setup projects

---
+++ IOSVideoGameLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSVideoGameLibrary
+//
+//  Created by Cristian Rodriguez on 4/12/18.
+//  Copyright © 2018 Cristian Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
